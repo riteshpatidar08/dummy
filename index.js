415cf3d273e848236c5762ce68114c99db2d35d5
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-07-15 18:35:55 +0530

    version1

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('version1') ;
+console.log('hello git')
+console.log('hello git 2')
+console.log('hello git 3')
+console.log('hello git 4')
---
+++ variables/variable.js
@@ -0,0 +1,3 @@
+console.log('version1');
+console.log('hello from variable file')
+console.log('hello from variable file 2')
