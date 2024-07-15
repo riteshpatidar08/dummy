140134873988748bfdd8cdc6e23544e9a174f60c
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-07-15 18:37:02 +0530

    version2

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-console.log('version1') ;
+console.log('version2') ;
 console.log('hello git')
 console.log('hello git 2')
 console.log('hello git 3')
 console.log('hello git 4')
+
+
+// commit === version 
