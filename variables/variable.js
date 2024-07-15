73a6823103108f9193240ba75b7701a4ff38f06b
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-07-15 18:44:16 +0530

    'new updated code"

--- variables/variable.js
+++ variables/variable.js
@@ -1,3 +1,11 @@
 console.log('version3');
 console.log('hello from variable file')
 console.log('hello from variable file 2')
+
+// 
+// git init for initialize empty folder
+//git status to check where we have make changes till from our last version
+//git add . // add the changed file to the staging area so we can create a new version o f our code 
+//git commit -m "msg"   //git commit is used to create a new version of our code 
+//git log to check the version of our code
+//git log --all to check all the previous version
